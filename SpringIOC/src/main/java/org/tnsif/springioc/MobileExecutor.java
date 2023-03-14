efa7c030b504ec7afca7dbe4b25896f314e90ca9
Dell <Dell@Sayali>
2023-03-14 12:52:13 +0530

    First commit

---
+++ SpringIOC/src/main/java/org/tnsif/springioc/MobileExecutor.java
@@ -0,0 +1,34 @@
+package org.tnsif.springioc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MobileExecutor {
+
+	public static void main(String[] args) {
+		//Hard Coding
+		/*Airtel a=new Airtel();
+		a.call();
+		a.message();
+		
+		Jio j = new Jio();
+		j.call();
+		j.message();*/
+		
+		ApplicationContext c = new ClassPathXmlApplicationContext("beans.xml");
+		
+		/*Airtel a=(Airtel) c.getBean("airtel");
+		a.call();
+		a.message();
+		
+		Jio b=(Jio) c.getBean("jio");
+		a.call();
+		a.message();*/
+		
+		Sim s =c.getBean("sim",Sim.class);
+		s.call();
+		s.message();
+	}
+	
+
+}
---
+++ SpringIOC/src/main/java/org/tnsif/springioc/Sim.java
@@ -0,0 +1,8 @@
+package org.tnsif.springioc;
+//program to demonstrate on spring ioc 
+public interface Sim {
+	//by default it is abstract 
+	void call();
+	void message();
+
+}
